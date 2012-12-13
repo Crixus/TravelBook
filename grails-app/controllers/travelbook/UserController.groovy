5700216f619c02d8905874d22e10ff44581f2e28
Crixus <jeremy.curny@gmail.com>
2012-12-13 09:57:09 +0100

    Ajout .gitignore + suppression classe User et Photo (User sera fait avec la security)

--- grails-app/controllers/travelbook/UserController.groovy
+++
@@ -1,6 +0,0 @@
-package travelbook
-
-class UserController {
-
-    def index() { }
-}
--- grails-app/domain/travelbook/Photo.groovy
+++
@@ -1,9 +0,0 @@
-package travelbook
-
-class Photo {
-
-	static belongsTo = [user: User]
-	
-    static constraints = {
-    }
-}
