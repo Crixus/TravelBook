c81ca91f8f51f63d646cda7da5aa77e0ce65dc12
PtiLu <ludo830@hotmail.fr>
2013-01-31 11:48:59 +0100

    Ajout du test instance pour la classe de domaine TravleTest

--- test/unit/travelbook/TravelTests.groovy
+++ test/unit/travelbook/TravelTests.groovy
@@ -1,7 +1,5 @@
 package travelbook
 
-
-
 import grails.test.mixin.*
 import org.junit.*
 
@@ -9,7 +7,7 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Travel)
-class TravelTests {
+class TravelTests implements IDomainClassDefaultTest {
 	
 	void testConstraint(){
 		
@@ -31,10 +29,27 @@ class TravelTests {
 		
 		travel = new Travel(destination:"Rio", dateDebut:today, dateFin:tomorrow, member:user)
 		assert travel.validate()
-		
 	}
-	
-    void testSomething() {
-       //fail "Implement me"
-    }
+
+	@Override
+	public void testInstance() {
+		
+		def today = new Date()
+		def tomorrow = today.next()
+		def yesterday = today.previous()
+		
+		def user = new User(username:"Deuxieme", password:"oulala123", lastName:"Campos", firstName:"Ludovic",email:"lolitude@hotmail.fr",
+			gender:"M", dateOfBirth:yesterday)
+		def travel = new Travel(destination:"Rio", dateDebut:today, dateFin:tomorrow, member:user)
+		
+		assert travel != null
+		
+		travel.setDestination("toto")
+		travel.setDateDebut(today.previous())
+		travel.setDateFin(tomorrow.previous())
+		
+		assert travel.getDestination()== "toto"
+		assert travel.getDateDebut()  == today.previous()
+		assert travel.getDateFin()    == tomorrow.previous()
+	}
 }
