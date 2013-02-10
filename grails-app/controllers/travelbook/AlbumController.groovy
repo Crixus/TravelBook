036bc8661917535a42c2292b8b9f102a1b93f1d5
abocoum <abocoum@hotmail.fr>
2013-02-10 17:47:07 +0100

    ok

--- grails-app/controllers/travelbook/AlbumController.groovy
+++ grails-app/controllers/travelbook/AlbumController.groovy
@@ -8,8 +8,6 @@ class AlbumController {
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
     def index() {
-		
-		
         redirect(action: "list", params: params)
     }
 
