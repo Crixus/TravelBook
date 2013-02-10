f576e0cb208a5e5071e802f51a92b4ea9dbdb5d3
abocoum <abocoum@hotmail.fr>
2013-02-10 16:47:01 +0100

    using spock 0.6 #17

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,6 +44,8 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
 		
+		test ":spock:0.6"
+		
 		//test(":spock:0.7") {
 		//	exclude "spock-grails-support"
 		//  }
